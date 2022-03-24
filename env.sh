7745058ccf02bebff273277cfa40f648d89077ee
cann484 <reilly.cannon@pnnl.gov>
2022-03-24 12:28:18 -0700

    removed metis

--- env.sh
+++ env.sh
@@ -4,7 +4,8 @@ module load gcc/9.1.0
 module load cmake/3.21.4
 
 # help IFOPT find IPOPT
-#export IPOPT_DIR=
+export IPOPT_DIR=$(pwd)/Ipopt
 export EIGEN3_ROOT=$(pwd)/include/eigen3
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$IPOPT_DIR/lib
 
 #TODO: wait for stack exchange up to add files to #include search path
--- setup_prereqs.sh
+++ setup_prereqs.sh
@@ -6,26 +6,25 @@ module load cmake/3.21.4
 export EIGEN3_ROOT=$(pwd)/include/eigen3
 export IPOPT_DIR=$(pwd)/Ipopt
 
-# build METIS
-cd ThirdParty-Metis
-./get.Metis
-./configure --prefix=$IPOPT_DIR
-make
-make install
-cd ..
-
 # build MUMPS
+echo ""
+echo "Building MUMPS"
+echo ""
 cd ThirdParty-Mumps
 ./get.Mumps
-./configure --with-metis --with-metis-lflags="-L${IPOPT_DIR}/lib -lcoinmetis" \
-     --with-metis-cflags="-I${IPOPT_DIR}/include -I${IPOPT_DIR}/include/coin-or -I${IPOPT_DIR}/include/coin-or/metis" \
-     --prefix=$IPOPT_DIR CFLAGS="-I${IPOPT_DIR}/include -I${IPOPT_DIR}/include/coin-or -I${IPOPT_DIR}/include/coin-or/metis" \
-     FCFLAGS="-I${IPOPT_DIR}/include -I${IPOPT_DIR}/include/coin-or -I${IPOPT_DIR}/include/coin-or/metis"
+./configure --prefix=$IPOPT_DIR CFLAGS="-I${IPOPT_DIR}/include -I${IPOPT_DIR}/include/coin-or" \
+     FCFLAGS="-I${IPOPT_DIR}/include -I${IPOPT_DIR}/include/coin-or"
 make
 make install
 cd ..
+echo ""
+echo "Finished MUMPS"
+echo ""
 
 # build IPOPT
+echo ""
+echo "Building IPOPT"
+echo ""
 cd $IPOPT_DIR
 mkdir build
 cd build
@@ -33,3 +32,18 @@ cd build
      --with-mumps-cflags="-I${IPOPT_DIR}/include/coin-or/mumps"
 make
 make install
+echo ""
+echo "Finished IPOPT"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$IPOPT_INSTALL_DIR/lib
+
+# for ifopt
+#export IPOPT_DIR=$(pwd)/Ipopt
+
+#build IFOPT
+echo ""
+echo "Building IFOPT"
+echo ""
+
+#cd ifopt
+#mkdir build && cd build
