7dfb812a46015be17322f5d1a0665974740beb91
cann484 <reilly.cannon@pnnl.gov>
2022-03-29 12:49:04 -0700

    working eigen and ifopt install

---
+++ clean_all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf lib64
+rm -rf include
+rm -rf build
+rm -rf eigen/build
+rm -rf ifopt/build
--- setup_prereqs.sh
+++ setup_prereqs.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-source env.sh
+source ./env.sh
 
 cd eigen
 rm -rf build
 mkdir build
 cd build
 cmake -DCMAKE_CXX_FLAGS="-std=c++17 " -DCMAKE_INSTALL_PREFIX=$(pwd)/../.. ..
+make install 
+
+cd ../..
+cd ifopt 
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="-std=c++17 " -DCMAKE_INSTALL_PREFIX=$(pwd)/../.. ..
+make install
+ cd ../..
 
